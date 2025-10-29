eb4adb8c513a201eefefb8ff91c6a5f5512c0bff
Luyen777 <luyen777@proton.me>
2025-10-29 11:38:16 +0100

    2

--- src/pages/BlackFridayMaldives.tsx
+++ src/pages/BlackFridayMaldives.tsx
@@ -411,7 +411,7 @@ const BlackFridayMaldives = () => {
               </div>
 
               {/* Carousel Section - Integrated into Hero */}
-              <div className="mt-12 -mx-6">
+              <div className="mt-6">
                 <CarouselSection />
               </div>
 
